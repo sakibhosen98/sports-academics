a4846e83e6e3cbdcf127bfc48116ccc3552580d5
sakibhosen98 <sakibhosen9815@gmail.com>
2023-06-10 03:15:53 +0600

    added addClass in database

--- src/Pages/Dashboard/AddClass.jsx
+++ src/Pages/Dashboard/AddClass.jsx
@@ -1,14 +1,151 @@
-import { Helmet } from "react-helmet-async";
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+import Swal from "sweetalert2";
+
 
 const AddClass = () => {
+  const {user} = useContext(AuthContext);
+  console.log(user)
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    const className = form.className.value;
+    const classImg = form.url.value;
+    const availableSeat = form.seat.value;
+    const price = form.price.value;
+
+    console.log(className, classImg, availableSeat, price)
+
+    const classes = {
+      className,
+      classImg,
+      availableSeat,
+      price,
+      instructorName: user?.displayName,
+      instructorEmail: user?.email
+    }
+
+    console.log(classes)
+
+    fetch('http://localhost:5000/instructors', {
+      method: 'POST',
+      headers: {
+       
+     'content-type': 'application/json'
+      },
+      body: JSON.stringify(classes)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.insertedId){
+        Swal.fire({
+          title: 'success!',
+          text: 'Class Added Successfully',
+          icon: 'success',
+          confirmButtonText: 'Cool'
+        })
+       }
+  
+    })
+
+
+
+    // fetch('http://localhost:5000/instructors', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(classes)
+    // })
+    // .then(res => res.json())
+    // .then(data => {
+    //   console.log(data)
+    // })
+
+    // Swal.fire({
+    //   title: 'User create successful',
+    //   showClass: {
+    //     popup: 'animate__animated animate__fadeInDown'
+    //   },
+    //   hideClass: {
+    //     popup: 'animate__animated animate__fadeOutUp'
+    //   }
+    // });
+
+  }
+
+    
+  
   return (
-    <div>
-      <Helmet>
-        <title>Starts Academies | AddClass</title>
-      </Helmet>
-      <h2>Add Class</h2>
+    <div className="bg-[#e4cda3]">
+      <h2 className="text-3xl font-bold py-5 text-center">Instructor Input Here</h2>
+      <form onSubmit={handleSubmit} className="p-8">
+        <div className="flex gap-3">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-xl font-semibold">Name</span>
+            </label>
+            <input
+              type="text"
+              name="className"
+              placeholder="Class Name"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-xl font-semibold">Image</span>
+            </label>
+            <input
+              type="url"
+              name="url"
+              placeholder="url"
+              className="input input-bordered"
+            />
+          </div>
+        </div>
+        <div className="flex gap-3">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-xl font-semibold">Available Seats</span>
+            </label>
+            <input
+              type="number"
+              name="seat"
+              placeholder="Seat"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-xl font-semibold">Price</span>
+            </label>
+            <input
+              type="number"
+              name="price"
+              placeholder="price"
+              className="input input-bordered"
+            />
+          </div>
+        </div>
+        <div className="form-control">
+            <input
+              type="submit"
+              value="Add Class"
+              className="btn bg-[#e0970f] mt-4"
+            />
+          </div>
+      </form>
     </div>
   );
 };
 
 export default AddClass;
+
+{
+  /* <Helmet>
+        <title>Starts Academies | AddClass</title>
+      </Helmet> */
+}
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,44 +3,39 @@ import { useForm } from "react-hook-form";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import Swal from "sweetalert2";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 const Login = () => {
-  const {signIn} = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
 
   const from = location.state?.from?.pathname || "/";
 
-  const {
-    register,
-    handleSubmit,
-  } = useForm();
+  const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
-    signIn(data.email, data.password)
-    .then((result) => {
+    signIn(data.email, data.password).then((result) => {
       const user = result.user;
       console.log(user);
       Swal.fire({
-        title: 'User Login in successful',
+        title: "User Login in successful",
         showClass: {
-          popup: 'animate__animated animate__fadeInDown'
+          popup: "animate__animated animate__fadeInDown",
         },
         hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
-        }
+          popup: "animate__animated animate__fadeOutUp",
+        },
       });
       navigate(from, { replace: true });
-    })
+    });
   };
 
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="">
-
-        </div>
+        <div className=""></div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <h2 className="text-center text-3xl pt-2 font-semibold">Login</h2>
           <form onSubmit={handleSubmit(onSubmit)} className="card-body">
@@ -67,11 +62,19 @@ const Login = () => {
               />
             </div>
             <div className="form-control mt-6">
-              <input type="submit" value="Login" className="btn bg-orange-500" />
+              <input
+                type="submit"
+                value="Login"
+                className="btn bg-orange-500"
+              />
             </div>
           </form>
-          <p><small>New Here? <Link to="/signup">Please Create an account</Link> </small></p>
-                        {/* <SocialLogin></SocialLogin> */}
+          <p>
+            <small>
+              New Here? <Link to="/signup">Please Create an account</Link>{" "}
+            </small>
+          </p>
+          <SocialLogin></SocialLogin>
         </div>
       </div>
     </div>
